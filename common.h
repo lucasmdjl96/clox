a7df14c144bf5fab6c05307d4569053a585a4ee9
Lucas M de Jong Larrarte <lucasmdjl96@gmail.com>
2023-08-16 15:21:51 +0200

    Chunks Of Bytecode + A Virtual Machine

---
+++ common.h
@@ -0,0 +1,14 @@
+//
+// Created by lucas on 8/13/23.
+//
+
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define DEBUG_TRACE_EXECUTION
+
+#endif //CLOX_COMMON_H
---
+++ vm.h
@@ -0,0 +1,33 @@
+//
+// Created by lucas on 8/15/23.
+//
+
+#ifndef CLOX_VM_H
+#define CLOX_VM_H
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM(VM* vm);
+void freeVM(VM* vm);
+InterpretResult interpret(VM* vm, Chunk* chunk);
+void push(VM* vm, Value value);
+Value pop(VM* vm);
+
+
+#endif //CLOX_VM_H
